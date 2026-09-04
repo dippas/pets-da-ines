f349b859f34411533096028944becb6b40ff8f10
Serpa <serpa14@gmail.com>
2026-09-04 14:47:40 +0100

    refactor: extract Header component, use Img wrapper everywhere

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,17 @@
+import InstagramLink from './InstagramLink'
+import LanguageSwitcher from './LanguageSwitcher'
 import LogoBand from './LogoBand'
 
 export default function Header() {
   return (
-    <header>
-      <LogoBand />
+    <header className="border-b-4 border-gold bg-lavender">
+      <div className="mx-auto flex max-w-6xl items-center justify-between gap-3 px-4 py-3 sm:px-8">
+        <LogoBand />
+        <div className="flex shrink-0 items-center gap-3 sm:gap-4">
+          <InstagramLink />
+          <LanguageSwitcher />
+        </div>
+      </div>
     </header>
   )
 }
